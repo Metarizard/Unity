ba7e3d201de8bf9e3fd9bf0851c88c440ae2c350
Marc Seguí Coll <metarizard@gmail.com>
2021-06-16 00:13:55 +0200

    Main menu and Car menu initial session
    Created the main menu, button and key press mechanics stablished in menu navigation. Sample car selection menu scene created too.

---
+++ MTZD Karting Simulator/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public static MenuManager Instance;
+
+    private GameObject titleScreen;
+    public GameObject mainMenuScreen;
+    void Start()
+    {
+        titleScreen = GameObject.Find("TitleScreen");
+        titleScreen.SetActive(true);
+        mainMenuScreen = GameObject.Find("MainMenuScreen");
+        mainMenuScreen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.Space)) && (titleScreen.activeInHierarchy))
+        {
+            titleScreen.SetActive(false);
+            mainMenuScreen.SetActive(true);
+        }
+    }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void SinglePlayer()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void TwoPlayers()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
